34a84adcbce3f4de3b06397ff24a271ec307e665
Paul Thomson <paul.thomson@sudosky.com>
2024-11-18 10:01:26 +0200

    --push

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -470,6 +470,35 @@ export interface ApiAuthorAuthor extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiCalculatorCalculator extends Struct.CollectionTypeSchema {
+  collectionName: 'calculators';
+  info: {
+    displayName: 'Calculators';
+    pluralName: 'calculators';
+    singularName: 'calculator';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    calculatorName: Schema.Attribute.String;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    iconURL: Schema.Attribute.String;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::calculator.calculator'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
   collectionName: 'categories';
   info: {
@@ -1260,6 +1289,7 @@ declare module '@strapi/strapi' {
       'api::about.about': ApiAboutAbout;
       'api::article.article': ApiArticleArticle;
       'api::author.author': ApiAuthorAuthor;
+      'api::calculator.calculator': ApiCalculatorCalculator;
       'api::category.category': ApiCategoryCategory;
       'api::global.global': ApiGlobalGlobal;
       'api::job-title.job-title': ApiJobTitleJobTitle;
